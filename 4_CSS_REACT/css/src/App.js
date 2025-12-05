20c272c80eba9380d67031d0276377c48904ce22
GABRIEL CARVALHO DOS SANTOS <01202010600>
2025-12-05 14:29:22 -0300

    new

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -5,8 +5,14 @@ import Mycomponent from './components/Mycomponent';
 function App() {
   return (
     <div className="App">
-        <h1>React Com CSS</h1>
+      {/* CSS GLOBAL*/}
+        <h1>React Com CSS "global"</h1>
+        {/* CSS DE COMPONENTE*/}
         <Mycomponent></Mycomponent>
+        {/* inline */}
+        <p>Este é o parágrafo do App.js</p>
+
+        <p style={{ color: 'blue', fontSize: '25px' }}>Este é o parágrafo estilizado com inline</p>
     </div>
   );
 }
--- 4_CSS_REACT/css/src/components/Mycomponent.js
+++ 4_CSS_REACT/css/src/components/Mycomponent.js
@@ -5,6 +5,8 @@ const Mycomponent = () => {
   return (
     <div>
         <h1>CSS DE COMPONENTE</h1>
+        <p>Este é o parágrafo do meu componente</p>
+        <p className="mycomp-paragraph">Este é o parágrafo estilizado especifico do meu componente</p>
     </div> 
   ) 
 };
